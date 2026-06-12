2bf30e8c9672abf094934edb6fa1251e3a2a47a2
Pivos-Marat <pivovarevichmarat@gmail.com>
2026-06-12 19:23:05 +0300

    f

---
+++ test/test01.cpp
@@ -0,0 +1,14 @@
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//int main() {
+//	int number;
+//	int* ptr = &number;
+//	int** dptr = &ptr;
+//
+//	cout << dptr  << " - " << *dptr << " - " << **dptr << endl;
+//	
+//
+//	return 0;
+//}
---
+++ test/test02.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int n, m;
+	
+
+	cout << "Input size of matrix: ";
+	cin >> n >> m;
+
+	int** matrix = new int*[n]
+
+		for (int i = 0; i < n; i++) {
+			*(matrix + i) = new int[m]
+		}
+
+
+	return 0;
+}
